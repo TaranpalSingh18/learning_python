3cd80315b55214bdddd39a64b3f1c292bdf2fb84
Taranpal <taranarora1470@gmail.com>
2025-07-28 15:12:15 +0530

    python learning day-4--> learnt about collections: counter

---
+++ collections/collection1.py
@@ -0,0 +1,15 @@
+# COLLECTIONS: counter, namedTuple, ordereddict, defaultdict, deque
+from collections import Counter
+s ="taaaaaarrrrrrrrrrrran"
+print(Counter(s))
+
+my_counter = Counter(s)
+print(my_counter.keys())
+
+print(my_counter.values())
+
+print(my_counter.items())
+
+print(my_counter.most_common(2))
+
+print(my_counter.most_common(2)[0][1])
